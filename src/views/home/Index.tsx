612d0778dcecfaa4a6f6e06ffebd333d1a7c1a7d
王双双 <635012489@qq.com>
2022-05-07 18:18:29 +0800

    1

--- src/views/home/Index.tsx
+++ src/views/home/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Index() {
   return (
     <div style={{fontSize:'20px'}}>
-      欢迎来到首页，整体项目权限基础框架已搭建好，后面可根据业务自行搭配其它模块
+      欢迎来到首页，整体项目权限基础框架已搭建好，后面可根据业务自行搭配其它模块进行扩展
     </div>
   )
 }
